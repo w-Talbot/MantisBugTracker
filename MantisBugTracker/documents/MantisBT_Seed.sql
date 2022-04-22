8828a3194f0b03d774309d33d5b8ff14f126b631
w-talbot <wesstalbot@gmail.com>
2022-04-22 15:37:12 +0100

    Added SQL file for seeding the database - enough to get started, but not finished.

---
+++ MantisBugTracker/documents/MantisBT_Seed.sql
@@ -0,0 +1,34 @@
+INSERT INTO mantisdb.mantis_user_table
+VALUES 
+(2, 'john doe', 'john doe', 'jd@test.test', '1234test' , 1, 0, 90,0,0,0,'5f4dcc3b5aa765d61d8327deb8Y2cf99','1650635623','1650635623').
+(3, 'jsmith', 'jane smith', 'js@test.test', '1234test' , 1, 0, 25,0,0,0,'5f4dcc3b5aa765d61d8327deH882cf99','1650635623','1650635623'),
+(4, 'aoehlen', 'albert oehlen', 'ao@test.test', '1234test' , 1, 0, 90,0,0,0,'5f4dcc3b5aa765d61d8327Jeb882cf99','1650635623','1650635623'),
+(5, 'akanevsky', 'alex kanevsky', 'ak@test.test', '1234test' , 1, 0, 25,0,0,0,'5f4dcc3b5aa765d6Fd8327deK882cf99','1650635623','1650635623'),
+(6, 'fbacon', 'francis bacon', 'fb@test.test', '1234test' , 1, 0, 25,0,0,0,'5f4dcc3b5aa765d61d832Mdeb882cf99','1650635623','1650635623'),
+(7, 'lfreud', 'lucian freud', 'lf@test.test', '1234test' , 1, 0, 25,0,0,0,'5f4dcc3b5aa765d61d8327deL882cf99','1650635623','1650635623')
+;
+
+INSERT INTO mantisdb.mantis_project_table
+VALUES 
+(1, 'RYTHM', 10, 1, 50, 10,'' ,'' , 1, 1),
+(2, 'GEOGRAPHY', 10, 1, 50, 10,'' , '', 1, 1),
+(3, 'RYTHM', 10, 1, 50, 10,'' ,'' , 1, 1),
+
+(4, 'GEOGRAPHY', 10, 1, 50, 10,'' , '', 1, 1),
+(5, 'RYTHM', 10, 1, 50, 10,'' ,'' , 1, 1),
+(6, 'GEOGRAPHY', 10, 1, 50, 10,'' , '', 1, 1),
+
+(7, 'SLOW', 10, 1, 50, 10,'' , '', 1, 1),
+(8, 'WAXY', 10, 1, 50, 10,'' , '', 1, 1),
+(9, 'DEMOTE', 10, 1, 50, 10,'','' , 1, 1),
+
+(10, 'INDONUT', 10, 1, 50, 10,'' ,'' , 1, 1),
+(11, 'HOPELESS', 10, 1, 50, 10,'' , '', 1, 1),
+(12, 'BROOMBROOM', 10, 1, 50, 10,'' ,'' , 1, 1),
+
+(13, 'MOUSE', 10, 1, 50, 10,'' , '', 1, 1),
+(14, 'DISTRUST', 10, 1, 50, 10,'' , '', 1, 1),
+(15, 'BLACK', 10, 1, 50, 10,'' ,'' , 1, 1),
+(16, 'ANTIFROZEN', 10, 1, 50, 10,'' ,'' , 1, 1),
+(17, 'CORNDOG', 10, 1, 50, 10,'' ,'' , 1, 1)
+;
